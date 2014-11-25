f1df8a9ed1f8ec544b46809994f90a6731d86ab4
Ricardo Padilha <ricardospadilha@gmail.com>
2014-11-25 11:53:36 +0100

    Fixes sudo segfault

--- build.sh
+++ build.sh
@@ -78,7 +78,7 @@ local URL="http://www.gratisoft.us/sudo/dist/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd target/"${FOLDER}"
-./configure --host=arm-none-linux-gnueabi --prefix="${DEST}" --sysconfdir="${DEST}/etc" --mandir="${DEST}/man" --with-vardir="/tmp/DroboApps/${NAME}/var" --with-iologdir="${DEST}/var/log/sudo-io" --with-rundir="/tmp/DroboApps/${NAME}/run" --with-editor="/bin/vi"
+./configure --host=arm-none-linux-gnueabi --prefix="${DEST}" --sysconfdir="${DEST}/etc" --mandir="${DEST}/man" --with-vardir="/tmp/DroboApps/${NAME}/var" --with-iologdir="${DEST}/var/log/sudo-io" --with-rundir="/tmp/DroboApps/${NAME}/run" --with-editor="/bin/vi" --disable-hardening
 make
 make install install_uid=$(id -u) install_gid=$(id -g)
 mv "${DEST}/etc/sudoers" "${DEST}/etc/sudoers.orig"
